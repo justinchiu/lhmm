a05b981c8f72498f7008942953dd4d2f9221c140
Justin T Chiu <jtc257@g2-login-01.coecis.cornell.edu>
2021-06-02 22:37:46 -0400

    music main

--- music_main.py
+++ music_main.py
@@ -43,7 +43,6 @@ def max_diff(log_p):
     xy = [(x,y) for x in range(n) for y in range(x, n)]
     diff = th.tensor([(P[x] - P[y]).abs().max() for x,y in xy])
     print(f"Max diff < 0.01: {(diff < 0.01).sum()} / {diff.shape[0]}")
-    #import pdb; pdb.set_trace()
 
 
 def update_best_valid(
@@ -77,12 +76,9 @@ def update_best_valid(
 def report(losses, n, prefix, start_time=None):
     loss = losses.evidence
     elbo = losses.elbo
-    # cap loss otherwise overflow
-    #loss = loss if loss > -1e7 else -1e7
     str_list = [
         f"{prefix}: log_prob = {loss:.2f}",
         f"xent(word) = {-loss / n:.2f}",
-        #f"ppl = {math.exp(-loss / n):.2f}",
     ]
     if elbo is not None:
         str_list.append(f"elbo = {elbo / n:.2f}")
@@ -117,7 +113,6 @@ def fast_eval_loop(
         # entropy
         # assert that transition and emission are well-formed
         myt = model.transition(print_max=True)
-        #myt = model.transition()
         bigt = myt.logsumexp(-1).abs().max()
         assert bigt < 1e-4, f"{bigt}"
         bige = emission.logsumexp(-1).abs().max()
@@ -202,7 +197,6 @@ def train_loop(
 ):
     global WANDB_STEP
 
-    noise_scales = np.linspace(1, 0, args.noise_anneal_steps)
     total_ll = 0
     total_elbo = 0
     n = 0
@@ -219,17 +213,6 @@ def train_loop(
             if args.iterator == "bucket":
                 lpz = None
                 last_states = None
-            #print(" ".join([model.V.itos[x] for x in text[0].tolist()]))
-
-            # set noise scale
-            if hasattr(model, "noise_scale"):
-                noise_scale = noise_scales[
-                    min(WANDB_STEP, args.noise_anneal_steps-1)
-                ] if args.noise_anneal_steps > 0 else model.init_noise_scale
-                model.noise_scale = noise_scale
-                wandb.log({
-                    "noise_scale": noise_scale,
-                }, step=WANDB_STEP)
 
             text, mask, lengths = batch
             n_tokens = mask.sum()
@@ -243,11 +226,6 @@ def train_loop(
             if hasattr(args, "eff") and args.eff:
                 losses, _, _= model.score_rff(
                     text, lpz=lpz, last_states=last_states, mask=mask, lengths=lengths)
-                """
-                losses2, _, _ = model.score(
-                    text, lpz=lpz, last_states=last_states, mask=mask, lengths=lengths)
-                import pdb; pdb.set_trace()
-                """
             else:
                 losses, lpz, last_states = model.score(
                     text, lpz=lpz, last_states=last_states, mask=mask, lengths=lengths)
@@ -328,22 +306,6 @@ def main():
     aux_device = th.device("cpu" if args.aux_devid < 0 else f"cuda:{args.aux_devid}")
     args.aux_device = aux_device
 
-    if args.dbg_double:
-        # test higher precision
-        th.set_default_tensor_type(th.cuda.DoubleTensor)
-        # reminder: never use 'as th' again
-
-    """
-    for data in [music.JSB_CHORALES, music.MUSE_DATA, music.NOTTINGHAM, music.PIANO_MIDI]:
-        d = music.load_data(data)
-        d_iter = MusicIterator(
-            dataset = (d["train"]["sequences"], d["train"]["sequence_lengths"]),
-            batch_size = 128,
-            sort_key = lambda x: x[1].sum(),
-        )
-        for b,m,l in d_iter:
-            print(b.shape)
-    """
     data_map = {
         "jsb": music.JSB_CHORALES,
         "muse": music.MUSE_DATA,
@@ -377,13 +339,7 @@ def main():
 
     if args.no_shuffle_train:
         train_iter.shuffle = False
-    #import pdb; pdb.set_trace()
-    """
-    args = get_config(args.model_config, device)
-    config = Pack(args.items() | args.items())
-    # for now, until migrate to argparse
-    config.model = args.type
-    """
+
     name = get_name(args, music=True)
     import tempfile
     wandb.init(project="music-hmm", name=name, config=args, dir=tempfile.mkdtemp())
@@ -393,15 +349,7 @@ def main():
     print(args)
 
     model = None
-    if args.model == "lstm":
-        model = LstmLm(V, args)
-    elif args.model == "hmm":
-        from models.hmmlm import HmmLm
-        model = HmmLm(V, args)
-    elif args.model == "lhmm":
-        from models.lhmmlm import LHmmLm
-        model = LHmmLm(V, args)
-    elif args.model == "blhmm":
+    if args.model == "blhmm":
         from music_models.blhmmlm import BLHmmLm
         model = BLHmmLm(V, args)
     elif args.model == "bandedhmm":
@@ -413,42 +361,6 @@ def main():
         # single projection doesnt work due to rank issues, try multiple projection
         #from models.msblhmmlm import MsblHmmLm
         #model = MsblHmmLm(V, args)
-    elif args.model == "sparsekernelhmm":
-        from models.sparse_kernel_hmmlm import SparseKernelHmmLm
-        model = SparseKernelHmmLm(V, args)
-    elif args.model == "ff":
-        model = FfLm(V, args)
-    elif args.model == "arhmm":
-        from models.arhmmlm import ArHmmLm
-        model = ArHmmLm(V, args)
-    elif args.model == "poehmm":
-        from models.poehmmlm import PoeHmmLm
-        model = PoeHmmLm(V, args)
-    elif args.model == "chmm":
-        from models.chmmlm import ChmmLm
-        model = ChmmLm(V, args)
-    elif args.model == "dhmm":
-        from models.dhmmlm import DhmmLm
-        model = DhmmLm(V, args)
-    elif args.model == "shmm":
-        from models.shmmlm import ShmmLm
-        model = ShmmLm(V, args)
-    elif args.model == "dhmm":
-        from models.dhmmlm import DhmmLm
-        model = DhmmLm(V, args)
-    elif args.model == "mshmm":
-        from models.mshmmlm import MshmmLm
-        model = MshmmLm(V, args)
-    elif args.model == "dshmm":
-        from models.dshmmlm import DshmmLm
-        model = DshmmLm(V, args)
-    elif args.model == "factoredhmm" and args.param == "scalar":
-        # hack for scalar
-        from models.newhmmlm import FactoredHmmLm
-        model = FactoredHmmLm(V, args)
-    elif args.model == "factoredhmm":
-        from models.factoredhmmlm import FactoredHmmLm
-        model = FactoredHmmLm(V, args)
     else:
         raise ValueError("Invalid model type")
     model.to(device)
@@ -469,17 +381,6 @@ def main():
         del past_model
         del past_chp
 
-    # augment training data
-    if "train_features" in args:
-        train_features = np.load(
-            args.train_features,
-            allow_pickle = True,
-        )
-        raise NotImplementedError
-
-    #DEBUG
-    #valid_losses, valid_n = mixed_cached_eval_loop(args, V, valid_iter, model)
-    #import pdb; pdb.set_trace()
     if args.eval_only:
         # make sure this is uncommented
         if args.eval_only != "none":
@@ -488,20 +389,7 @@ def main():
         dump_transition(model)
 
         v_start_time = time.time()
-        #valid_losses, valid_n = eval_loop(
-        #valid_losses, valid_n = cached_eval_loop(
-        if args.model == "mshmm" or args.model == "factoredhmm":
-            if args.num_classes > 2 ** 15:
-                eval_fn = mixed_cached_eval_loop
-            else:
-                eval_fn = cached_eval_loop
-        elif args.model == "hmm" or args.model =="lhmm":
-            eval_fn = cached_eval_loop
-        elif args.model == "blhmm" or args.model == "sblhmm" or args.model == "bandedhmm":
-            eval_fn = fast_eval_loop
-        else:
-            eval_fn = eval_loop
-        #eval_fn = cached_eval_loop if args.model == "mshmm" else eval_loop
+        eval_fn = fast_eval_loop
         valid_losses, valid_n = eval_fn(
             args, V, valid_iter, model,
         )
@@ -518,12 +406,6 @@ def main():
             args, V, test_iter, model,
         )
         report(test_losses, test_n, f"Test perf", t_start_time)
-
-        # count states
-        #test_counts = collect_counts_loop(
-            #args, V, test_iter, model,
-        #)
-        #test_counts.print_counts()
 
         sys.exit()
 
@@ -612,46 +494,6 @@ def main():
             "epoch": e,
         }, step=WANDB_STEP)
 
-        if args.log_counts > 0 and args.keep_counts > 0:
-            # TODO: FACTOR OUT
-            # only look at word tokens
-            counts = (model.counts / model.counts.sum(0, keepdim=True))[:,4:]
-            c, v = counts.shape
-            #cg4 = counts > 1e-4
-            #cg3 = counts > 1e-3
-            cg2 = counts > 1e-2
-
-            # state counts
-            # log these once per epoch, then set back to zero
-            sc0 = (model.state_counts == 0).sum()
-            sc1 = (model.state_counts == 1).sum()
-            sc2 = (model.state_counts == 2).sum()
-            sc3 = (model.state_counts == 3).sum()
-            sc4 = (model.state_counts == 4).sum()
-            sc5 = (model.state_counts >= 5).sum()
-
-            wandb.log({
-                #"avgcounts@1e-4": cg4.sum().item() / float(v),
-                #"avgcounts@1e-3": cg3.sum().item() / float(v),
-                "avgcounts@1e-2": cg2.sum().item() / float(v),
-                #"maxcounts@1e-4": cg4.sum(0).max().item() / float(v),
-                #"maxcounts@1e-3": cg3.sum(0).max().item() / float(v),
-                "maxcounts@1e-2": cg2.sum(0).max().item(),
-                #"mincounts@1e-4": cg4.sum(0).min().item() / float(v),
-                #"mincounts@1e-3": cg3.sum(0).min().item() / float(v),
-                "mincounts@1e-2": cg2.sum(0).min().item(),
-                "maxcounts": counts.sum(0).max().item(),
-                "mincounts": counts.sum(0).min().item(),
-
-                "statecounts=0": sc0,
-                "statecounts=1": sc1,
-                "statecounts=2": sc2,
-                "statecounts=3": sc3,
-                "statecounts=4": sc4,
-                "statecounts>=5": sc5,
-            }, step=WANDB_STEP)
-            del cg2
-            del counts
 
     # won't use best model. Rerun with eval_only
     t_start_time = time.time()
